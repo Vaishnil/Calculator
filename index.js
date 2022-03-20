f498dd7cb3954fe43839acf431b3f45a1808e96c
Vaishnil <101132489+Vaishnil@users.noreply.github.com>
2022-03-20 23:41:57 +0530

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-console.log("running")
-document.querySelector('.close').style.display ="none";
-document.querySelector('.hamberger').addEventListener("click",()=>{
-    document.querySelector('sidebar').classList.toggle('sidebarGo');
-})
-
